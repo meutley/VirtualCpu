36e4ba34f0a7844f86eb47c9d412abb3d7e3f5c9
Matthew Utley <matthew.utley@availity.com>
2017-05-07 14:43:54 -0500

    Simple and working ASM interpreter implementation

--- src/TestApp/Assembler.cpp
+++ src/TestApp/Assembler.cpp
@@ -9,6 +9,8 @@ const string Assembler::SET = string("SET");
 const string Assembler::ADD = string("ADD");
 const string Assembler::SUBTRACT = string("SUBTRACT");
 
+const byte Assembler::OPERAND_SEPARATOR = ',';
+
 
 Assembler::Assembler()
 {
@@ -21,46 +23,33 @@ Assembler::~Assembler()
 
 /* ========== Main ========== */
 
-vector<byte>& Assembler::Assemble(const vector<string>& pCode)
+vector<byte> Assembler::Assemble(const vector<string>& pCode)
 {
+	m_currentLine = 0;
 	for (auto it = pCode.begin(); it != pCode.end(); it++)
 	{
-		vector<string> line = this->Split(*it);
+		auto line = this->Split(Trim(*it));
+		auto combinedOperands = CombineStrings(line, 1);
+		auto operands = ParseOperands(combinedOperands);
 		
 		auto opCode = line[0];
 		if (opCode == Assembler::SET)
 		{
-			if (line.size() != 3)
+			if (!ParseSet(operands))
 			{
-				cout << "ERROR: Expected opcode and two operands, but got " << line.size() << " :: " << *it << endl;
 				return vector<byte>();
 			}
-
-			auto leftOperand = line[1].substr(0, line[1].find_first_of(','));
-			auto rightOperand = stoi(line[2]);
-
-			m_instructions.push_back(Cpu::SET);
-			m_instructions.push_back(CpuRegisterMap.find(leftOperand)->second);
-			m_instructions.push_back(rightOperand);
 		}
 		else if (opCode == Assembler::ADD)
 		{
-			if (line.size() != 3)
+			if (!ParseAdd(operands))
 			{
-				cout << "ERROR: Expected opcode and two operands, but got " << line.size() << " :: " << *it << endl;
 				return vector<byte>();
 			}
-
-			auto leftOperand = line[1].substr(0, line[1].find_first_of(','));
-			auto rightOperand = line[2];
-
-			m_instructions.push_back(Cpu::ADD);
-			m_instructions.push_back(CpuRegisterMap.find(leftOperand)->second);
-			m_instructions.push_back(CpuRegisterMap.find(rightOperand)->second);
 		}
-	}
 
-	DumpInstructions();
+		m_currentLine++;
+	}
 
 	return m_instructions;
 }
@@ -89,4 +78,191 @@ vector<string> Assembler::Split(const string& pInput)
 	}
 	
 	return result;
+}
+
+string Assembler::Trim(const string& input)
+{
+	if (input.length() == 0)    // Empty, nothing to do
+	{
+		return input;
+	}
+
+	// Trim trailing and leading spaces
+	string output = input;
+	auto leftPosition = input.find_first_not_of(' ');
+	auto rightPosition = input.find_last_not_of(' ');
+	
+	// Return untouched input string if no spaces found to trim
+	if (leftPosition > input.length() && rightPosition > input.length())
+	{
+		return input;
+	}
+	
+	return output.substr(leftPosition, rightPosition + 1);
+}
+
+string Assembler::CombineStrings(const vector<string>& input, const int& startFrom)
+{
+	string result;
+
+	if (startFrom > input.size())
+	{
+		return result;
+	}
+
+	for (auto it = input.begin() + startFrom; it != input.end(); it++)
+	{
+		result.append(*it);
+	}
+
+	return result;
+}
+
+vector<string> Assembler::ParseOperands(const string& input, const byte& separator)
+{
+	vector<string> result;
+
+	auto trimmed = Trim(input);
+	if (trimmed.length() == 0)
+	{
+		return result;
+	}
+
+	string currentOperand;
+	int index = 0;
+	while (true)
+	{
+		if (index == trimmed.length())    // End of input
+		{
+			if (currentOperand.length() > 0 && Trim(currentOperand).length() > 0)
+			{
+				result.push_back(currentOperand);
+			}
+
+			break;
+		}
+
+		while (trimmed[index] == ' ')    // Skip whitespace
+		{
+			index++;
+			continue;
+		}
+
+		if (trimmed[index] == separator)    // Operand parsed; look for next operand
+		{
+			if (Trim(currentOperand).length() > 0)
+			{
+				result.push_back(currentOperand);
+			}
+
+			currentOperand = "";
+			index++;
+			continue;
+		}
+
+		currentOperand += trimmed[index];
+		index++;
+	}
+	
+	return result;
+}
+
+bool Assembler::CheckOperands(const vector<string>& operands, const int& expectedCount)
+{
+	if (operands.size() != expectedCount)
+	{
+		cout << "ERROR: Expected opcode and " << expectedCount << " operand(s), but got " << operands.size() << " at Line " << (m_currentLine + 1) << endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool Assembler::IsRegisterValid(const string& registerName)
+{
+	if (CpuRegisterMap.find(registerName) == CpuRegisterMap.end())
+	{
+		cout << "ERROR: Register name " << registerName << " not found at line " << (m_currentLine + 1) << endl;
+		return false;
+	}
+
+	return true;
+}
+
+const byte& Assembler::GetRegisterId(const string& name)
+{
+	if (IsRegisterValid(name))
+	{
+		return CpuRegisterMap.find(name)->second;
+	}
+
+	return byte();
+}
+
+void Assembler::AddInstruction(const byte& instruction, const byte& leftOperand, const byte& rightOperand)
+{
+	m_instructions.push_back(instruction);
+	m_instructions.push_back(leftOperand);
+	m_instructions.push_back(rightOperand);
+}
+
+bool Assembler::ParseSet(const vector<string>& operands)
+{
+	if (CheckOperands(operands, 2))
+	{
+		try
+		{
+			auto leftOperand = operands[0];
+			auto rightOperand = stoi(operands[1]);
+
+			if (IsRegisterValid(leftOperand))
+			{
+				AddInstruction(Cpu::SET, GetRegisterId(leftOperand), rightOperand);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (invalid_argument invalidArg)
+		{
+			cout << "ERROR: INVALID OPERAND" << endl;
+			cout << "   Right operand must be a valid integer, current = " << operands[1] << endl;
+			cout << "   LINE: " << (m_currentLine + 1) << endl;
+			return false;
+		}
+		catch (out_of_range outOfRange)
+		{
+			cout << "ERROR: INVALID OPERAND" << endl;
+			cout << "   Right operand out of range(max = 255), current = " << operands[1] << endl;
+			cout << "   LINE: " << (m_currentLine + 1) << endl;
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Assembler::ParseAdd(const vector<string>& operands)
+{
+	if (CheckOperands(operands, 2))
+	{
+		auto leftOperand = operands[0];
+		auto rightOperand = operands[1];
+
+		if (IsRegisterValid(leftOperand) && IsRegisterValid(rightOperand))
+		{
+			AddInstruction(Cpu::ADD, GetRegisterId(leftOperand), GetRegisterId(rightOperand));
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+	return true;
 }
--- src/TestApp/Assembler.h
+++ src/TestApp/Assembler.h
@@ -15,18 +15,39 @@ public:
 	Assembler();
 	~Assembler();
 
+
 public:    // Main
-	vector<byte>& Assemble(const vector<string>& pCode);
+	vector<byte> Assemble(const vector<string>& pCode);
 	void DumpInstructions();
+
 
 private:    // Code
 	vector<string> m_code;
+	unsigned int m_currentLine;
 
 	vector<string> Split(const string& pInput);
+	string Trim(const string& input);
+	string CombineStrings(const vector<string>& input, const int& startFrom = 0);
+	vector<string> ParseOperands(const string& input, const byte& separator = Assembler::OPERAND_SEPARATOR);
+
+	bool CheckOperands(const vector<string>& operands, const int& expectedCount);
+	bool IsRegisterValid(const string& registerName);
 
 	static const string SET;
 	static const string ADD;
 	static const string SUBTRACT;
+
+	static const byte OPERAND_SEPARATOR;
+
+
+private:    // Instructions and CPU
+	const byte& GetRegisterId(const string& name);
+
+	void AddInstruction(const byte& instruction, const byte& leftOperand, const byte& rightOperand);
+
+	bool ParseSet(const vector<string>& operands);
+	bool ParseAdd(const vector<string>& operands);
+
 
 private:    // Output
 	vector<byte> m_instructions;
